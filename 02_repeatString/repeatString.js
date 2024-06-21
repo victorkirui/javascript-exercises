3d5c1f941954e0ec042dd5107c76d424224af0ed
victorkirui <vkkiprop@gmail.com>
2024-06-21 09:24:21 +0300

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let newStr = "";
+  if (num < 0) {
+    newStr = "ERROR";
+  }
+  for (let i = 0; i < num; i++) {
+    newStr += string;
+  }
+  return newStr;
 };
 
 // Do not edit below this line
