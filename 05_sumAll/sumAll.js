5b945ac818c8ebebe9642fcc888065807adcc021
victorkirui <vkkiprop@gmail.com>
2024-06-23 17:20:01 +0300

    Pass all TDD(Test Driven Development) tests for the sumall exercise :q exit
    :wq
    quit
    wq
    :wq
    
    :
    :.
    git commit -m

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
 
+  let large = num1 > num2 ? num1 : num2;
+  let small = num1 < num2 ? num1 : num2;
+
+  if (large < 0 || small < 0) {
+    sum = "ERROR";
+  } else if (typeof large !== "number" || typeof small !== "number") {
+    sum = "ERROR";
+  } else {
+    for (let i = small; i <= large; i++) {
+      sum += i;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
