7a79f8c6729544cfc51ed1af05895619cb795777
victorkirui <vkkiprop@gmail.com>
2024-06-23 16:59:32 +0300

    Remove rest parameters from arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...numbers) {
+  let newArray = arr.filter((item) => !numbers.includes(item));
+
+  return newArray;
 };
 
 // Do not edit below this line
