bd98ad447cae153c1f585fbc5b92a1a1331cf745
victorkirui <vkkiprop@gmail.com>
2024-08-02 12:26:21 +0300

    Solve fibonacci sequence exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let count = typeof num !== "number" ? parseInt(num) : num;
 
+  if (count === 0) {
+    return 0;
+  }
+  if (count < 0) {
+    return "OOPS";
+  }
+
+  let n1 = 1,
+    n2 = 1,
+    nextN;
+
+  for (let i = 2; i <= count; i++) {
+    nextN = n1 + n2;
+    n1 = n2;
+    n2 = nextN;
+  }
+
+  return n1;
 };
 
 // Do not edit below this line
