7c785e351b7ae39c818fe04713643024c096e4ae
victorkirui <vkkiprop@gmail.com>
2024-08-02 11:05:14 +0300

    Solve palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  let cleanedString = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanumerical.includes(char))
+    .join("");
+
+  let reversedStr = cleanedString.split("").reverse().join("");
+
+  return cleanedString === reversedStr;
 };
 
 // Do not edit below this line
